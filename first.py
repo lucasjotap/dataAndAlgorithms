01bcc70872e834fd041be6a87cbce1e110a84dae
lucasjotap <leiathemosthot@gmail.com>
2022-09-16 17:11:06 -0300

    Data and algorithms

---
+++ first.py
@@ -0,0 +1,15 @@
+# Variables, data and their types.
+
+# Data is a sequence of symbols that are quantified or quantifiable.
+
+# Variables are addresses for memory clusters.
+
+# Writing variables, addressing data to memory.
+
+grade = 'a'
+classSubject = "Logic for programming and algorithms"
+
+# Displaying variables on console.
+print(classSubject + ": Student a's grade: ", 
+        grade)
+

d2497483837dcf853c4ca0c1123281e161915b86
lucasjotap <leiathemosthot@gmail.com>
2022-09-16 19:00:22 -0300

    Problem A in Python

--- firstProblem.py
+++ firstProblem.py
@@ -6,10 +6,13 @@
 def summing(args, kwargs):
     print("Welcome to the first problem, we'll request two integers from you.")
 
+    # Setting up functions's attibutes
     args = int(input("Please select any integer for this exercise: "))
     kwargs = int(input("Another integer of you choice: "))
-    return args + kwargs
+    print ("These were the integers you've chosen: %i and %i" % (args, kwargs))
+    phrase = f"When you add these values you get: {args + kwargs}"
+    return phrase
 
+# Calling funtion in
 x = summing(1, 2)
-
 print(x)

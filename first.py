5debb465dd8203b596b6a8aca81cdff08cc1c63b
lucasjotap <leiathemosthot@gmail.com>
2022-09-16 18:17:31 -0300

    Slicing variables in Python

--- first.py
+++ first.py
@@ -1,9 +1,6 @@
 # Variables, data and their types.
-
 # Data is a sequence of symbols that are quantified or quantifiable.
-
 # Variables are addresses for memory clusters.
-
 # Writing variables, addressing data to memory.
 
 grade = 'a'
@@ -11,5 +8,5 @@ classSubject = "Logic for programming and algorithms"
 
 # Displaying variables on console.
 print(classSubject + ": Student a's grade: ", 
-        grade)
-
+    grade)
+ 
---
+++ slicing.py
@@ -0,0 +1,5 @@
+# You can slice strings and work the data within them.
+var = "Logic and Programming"
+
+# Print the first word that belongs to the variable 'var'
+print(var[10:21], var[0:5])

0ce4db2c82a1bc9f844891c7ef3053ed7e13d270
lucasjotap <leiathemosthot@gmail.com>
2022-09-16 17:58:03 -0300

    Data Structures in Python

---
+++ dataStructure.py
@@ -0,0 +1,11 @@
+# The most basic form of a data structure is a list
+
+def create_list(args, kwargs):
+    """Function for data structure exercise"""
+
+    random_list = list(range(args, kwargs))
+    return random_list
+
+# Displaying data structure on console.
+describe = create_list(1, 20)
+print(describe)

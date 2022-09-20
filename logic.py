f0e63fb9981260e0d6f10e3f1df31fd7b573cb0b
lucasjotap <leiathemosthot@gmail.com>
2022-09-20 15:04:39 -0300

    Learning logic/bin/python3 /home/lucas/Desktop/algorithmsClass/logic.py

--- logic.py
+++ logic.py
@@ -8,3 +8,9 @@ z = 9
 print(x > y and z == x)
 # The result here is true since both premises are true.
 print(x > y and z < x)
+
+# Using the or operator you can get different results as well.
+# The output here is true because only one premise needs to be true.
+print("\n" + str(y > x or z < x))
+# The output here is false because at least one premise needs to be true.
+print(y > x or x < z)
